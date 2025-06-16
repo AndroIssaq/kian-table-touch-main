0f0f8dc7ed2bf353eece3fb9890a079fee7e3c11
Ibrahim-Rezq <ovic391@gmail.com.com>
2025-06-16 23:41:41 +0300

    Remove cleark from dep and clerkprovider

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,6 @@ import UsersDashboard from '@/pages/UsersDashboard'
 import VerificationCode from '@/pages/VerificationCode'
 
 // Contexts
-import { AdminAuthProvider } from '@/contexts/AdminAuthContext'
 import { CartProvider } from '@/contexts/CartContext'
 import { LanguageProvider } from '@/contexts/LanguageContext'
 import { LoadingProvider } from '@/contexts/LoadingContext'
@@ -42,152 +41,134 @@ const queryClient = new QueryClient()
 
 function App() {
     return (
-        <ClerkProvider
-            localization={arSA}
-            telemetry={false}
-            publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}
-            appearance={{
-                variables: {
-                    colorPrimary: '#d39d28',
-                    colorText: '#d39d28',
-                    colorBackground: '#d39d28',
-                    colorInputBackground: '#d39d28',
-                    colorInputText: '#d39d28',
-                    colorDanger: '#d39d28',
-                    fontFamily: 'Cairo, sans-serif',
-                    borderRadius: '1.5rem',
-                },
-            }}
-        >
-            <QueryClientProvider client={queryClient}>
-                <LoadingProvider>
-                    <CentralLoader />
-                    <ThemeProvider>
-                        <LanguageProvider>
-                            <CartProvider>
-                                <AuthProvider>
-                                    <LoyaltyPointsProvider>
-                                        <Toaster />
-                                        <Routes>
-                                            <Route
-                                                path='/'
-                                                element={<VerificationCode />}
-                                            />
-                                            {/* Protected User Routes */}
-                                            <Route
-                                                path='/user-home'
-                                                element={
-                                                    <ProtectedRoute>
-                                                        <UserHome />
-                                                    </ProtectedRoute>
-                                                }
-                                            />
+        <QueryClientProvider client={queryClient}>
+            <LoadingProvider>
+                <CentralLoader />
+                <ThemeProvider>
+                    <LanguageProvider>
+                        <CartProvider>
+                            <AuthProvider>
+                                <LoyaltyPointsProvider>
+                                    <Toaster />
+                                    <Routes>
+                                        <Route
+                                            path='/'
+                                            element={<VerificationCode />}
+                                        />
+                                        {/* Protected User Routes */}
+                                        <Route
+                                            path='/user-home'
+                                            element={
+                                                <ProtectedRoute>
+                                                    <UserHome />
+                                                </ProtectedRoute>
+                                            }
+                                        />
 
-                                            {/* Protected User Routes */}
-                                            <Route
-                                                path='/choose-table'
-                                                element={
-                                                    <ProtectedRoute>
-                                                        <ChooseTable />
-                                                    </ProtectedRoute>
-                                                }
-                                            />
-                                            <Route
-                                                path='/menu'
-                                                element={
-                                                    <ProtectedRoute>
-                                                        <Menu />
-                                                    </ProtectedRoute>
-                                                }
-                                            />
-                                            <Route
-                                                path='/call-waiter'
-                                                element={
-                                                    <ProtectedRoute>
-                                                        <CallWaiter />
-                                                    </ProtectedRoute>
-                                                }
-                                            />
-                                            <Route
-                                                path='/menu/category/:id'
-                                                element={<CategoryMenu />}
-                                            />
+                                        {/* Protected User Routes */}
+                                        <Route
+                                            path='/choose-table'
+                                            element={
+                                                <ProtectedRoute>
+                                                    <ChooseTable />
+                                                </ProtectedRoute>
+                                            }
+                                        />
+                                        <Route
+                                            path='/menu'
+                                            element={
+                                                <ProtectedRoute>
+                                                    <Menu />
+                                                </ProtectedRoute>
+                                            }
+                                        />
+                                        <Route
+                                            path='/call-waiter'
+                                            element={
+                                                <ProtectedRoute>
+                                                    <CallWaiter />
+                                                </ProtectedRoute>
+                                            }
+                                        />
+                                        <Route
+                                            path='/menu/category/:id'
+                                            element={<CategoryMenu />}
+                                        />
 
-                                            {/* Admin Auth Route */}
-                                            <Route
-                                                path='/admin-auth'
-                                                element={<AdminAuth />}
-                                            />
+                                        {/* Admin Auth Route */}
+                                        <Route
+                                            path='/admin-auth'
+                                            element={<AdminAuth />}
+                                        />
 
-                                            {/* Protected Admin Routes */}
-                                            <Route
-                                                path='/staff-dashboard'
-                                                element={
-                                                    <AdminProtectedRoute>
-                                                        <StaffDashboard />
-                                                    </AdminProtectedRoute>
-                                                }
-                                            />
-                                            <Route
-                                                path='/loyalty-management'
-                                                element={
-                                                    <AdminProtectedRoute>
-                                                        <LoyaltyManagement />
-                                                    </AdminProtectedRoute>
-                                                }
-                                            />
+                                        {/* Protected Admin Routes */}
+                                        <Route
+                                            path='/staff-dashboard'
+                                            element={
+                                                <AdminProtectedRoute>
+                                                    <StaffDashboard />
+                                                </AdminProtectedRoute>
+                                            }
+                                        />
+                                        <Route
+                                            path='/loyalty-management'
+                                            element={
+                                                <AdminProtectedRoute>
+                                                    <LoyaltyManagement />
+                                                </AdminProtectedRoute>
+                                            }
+                                        />
 
-                                            {/* Reports Route */}
-                                            <Route
-                                                path='/reports'
-                                                element={
-                                                    <AdminProtectedRoute>
-                                                        <Reports />
-                                                    </AdminProtectedRoute>
-                                                }
-                                            />
-                                            <Route
-                                                path='/menu-dashboard'
-                                                element={
-                                                    <AdminProtectedRoute>
-                                                        <MenuDashboard />
-                                                    </AdminProtectedRoute>
-                                                }
-                                            />
-                                            <Route
-                                                path='/users-dashboard'
-                                                element={
-                                                    <AdminProtectedRoute>
-                                                        <UsersDashboard />
-                                                    </AdminProtectedRoute>
-                                                }
-                                            />
-                                            <Route
-                                                path='/menu-dashboard/category/:id'
-                                                element={<CategoryProducts />}
-                                            />
-                                            {/* Other routes */}
-                                            <Route
-                                                path='*'
-                                                element={<NotFound />}
-                                            />
-                                            <Route
-                                                path='/sign-in/*'
-                                                element={<SignInPage />}
-                                            />
-                                            <Route
-                                                path='/sign-up/*'
-                                                element={<SignUpPage />}
-                                            />
-                                        </Routes>
-                                    </LoyaltyPointsProvider>
-                                </AuthProvider>
-                            </CartProvider>
-                        </LanguageProvider>
-                    </ThemeProvider>
-                </LoadingProvider>
-            </QueryClientProvider>
-        </ClerkProvider>
+                                        {/* Reports Route */}
+                                        <Route
+                                            path='/reports'
+                                            element={
+                                                <AdminProtectedRoute>
+                                                    <Reports />
+                                                </AdminProtectedRoute>
+                                            }
+                                        />
+                                        <Route
+                                            path='/menu-dashboard'
+                                            element={
+                                                <AdminProtectedRoute>
+                                                    <MenuDashboard />
+                                                </AdminProtectedRoute>
+                                            }
+                                        />
+                                        <Route
+                                            path='/users-dashboard'
+                                            element={
+                                                <AdminProtectedRoute>
+                                                    <UsersDashboard />
+                                                </AdminProtectedRoute>
+                                            }
+                                        />
+                                        <Route
+                                            path='/menu-dashboard/category/:id'
+                                            element={<CategoryProducts />}
+                                        />
+                                        {/* Other routes */}
+                                        <Route
+                                            path='*'
+                                            element={<NotFound />}
+                                        />
+                                        <Route
+                                            path='/sign-in/*'
+                                            element={<SignInPage />}
+                                        />
+                                        <Route
+                                            path='/sign-up/*'
+                                            element={<SignUpPage />}
+                                        />
+                                    </Routes>
+                                </LoyaltyPointsProvider>
+                            </AuthProvider>
+                        </CartProvider>
+                    </LanguageProvider>
+                </ThemeProvider>
+            </LoadingProvider>
+        </QueryClientProvider>
     )
 }
 
